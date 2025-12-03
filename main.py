e407a5c33a57508d1dee788f8bdb2b2c8f9d8b8d
wy85-bit <149866791+wy85-bit@users.noreply.github.com>
2025-12-03 21:34:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+
 import time
 import pandas as pd
 import os
