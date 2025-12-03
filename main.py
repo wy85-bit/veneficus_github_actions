f094a1e4a2c7777f359a0dae3076b28a74c8c82e
wy85-bit <149866791+wy85-bit@users.noreply.github.com>
2025-12-03 21:40:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,141 +2,110 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
 import time
 import pandas as pd
-import os
-from dotenv import load_dotenv
-from selenium.webdriver.common.by import By
 import json
+import os
 
-load_dotenv()
+def get(url, driver):
+    driver.get(url)
+    time.sleep(1)
 
-def go_to_main_page_search_term_data(driver):
-    driver.get('https://www.circle8.nl/zoeken?query=data')
-    wait_for_vacancies(driver)
-    
-def go_to_main_page_search_term_data_engineer(driver):
-    driver.get('https://www.circle8.nl/zoeken?query=data%20engineer')
-    wait_for_vacancies(driver)
-    
-def go_to_main_page_search_term_data_machine_learning_engineer(driver):
-    driver.get('https://www.circle8.nl/zoeken?query=machine%20learning%20engineer')
-    wait_for_vacancies(driver)
-    
+def go_to(driver, term):
+    get(f"https://www.circle8.nl/zoeken?query={term}", driver)
+
 def list_vacancy_links(driver):
     elements = driver.find_elements(By.XPATH, '//div[@class="c-vacancy-list"]//a[starts-with(@href, "/opdracht")]')
-    links = [element.get_attribute("href") for element in elements]
-    return links
+    return [e.get_attribute("href") for e in elements]
 
 def list_pagination_links(driver):
     elements = driver.find_elements(By.XPATH, '//div[@class="c-lister-pagination__wrapper"]//a')
-    pagination_links = [element.get_attribute("href") for element in elements]
-    return pagination_links
-
-def wait_for_vacancies(driver):
-    WebDriverWait(driver, 10).until(
-        EC.presence_of_all_elements_located(
-            (By.XPATH, '//a[starts-with(@href, "/opdracht")]')
-        )
-    )
+    return [e.get_attribute("href") for e in elements]
 
 def scrape(driver):
-    df_rows = []
-    new_df = pd.DataFrame()    
-    for page in list_pagination_links(driver):
-            print(page)
-            driver.get(page)
-            time.sleep(2)
-            for vacancy in list_vacancy_links(driver):
-                driver.get(vacancy)
-                time.sleep(2)
+    rows = []
 
-                UID = driver.find_element(By.XPATH, '//p[@class="c-vacancy-hero__jobId detail"]').text
+    pages = list_pagination_links(driver)
+    if not pages:
+        pages = [driver.current_url]
 
-                main_information_elements = driver.find_elements(By.XPATH, '//div[@class="c-vacancy-hero__usp-container"]/div/div[count(@*)=0]')
-                main_information = [element.text for element in main_information_elements]
+    for page in pages:
+        get(page, driver)
 
-                plaats = main_information[0]
-                duur = main_information[1]
-                uren = main_information[2]
-                start = main_information[3]
-                deadline = main_information[4]
+        vacancies = list_vacancy_links(driver)
+        for vacancy in vacancies:
+            get(vacancy, driver)
 
-                vacature_text= driver.find_elements(By.CSS_SELECTOR, 'body > main > div.c-vacancy-paragraph__outer-wrapper.outer-wrapper.background.primary > div > div.c-vacancy-paragraph__text-container > div.c-vacancy-paragraph__body-text.body-text > div:nth-child(1)')
-                vacature_text_elements = vacature_text[0].text   
-                
-                eisen_elements = driver.find_elements(By.XPATH, '//div[h3[text()="Eisen:"]]/ul//li')
-                eisen = [element.text for element in eisen_elements]
+            UID = driver.find_element(By.XPATH, '//p[@class="c-vacancy-hero__jobId detail"]').text
 
-                wensen_elements = driver.find_elements(By.XPATH, '//div[h3[text()="Wensen:"]]/ul//li')
-                wensen = [element.text for element in wensen_elements]
+            main_info = [
+                e.text for e in driver.find_elements(
+                    By.XPATH,
+                    '//div[@class="c-vacancy-hero__usp-container"]/div/div[count(@*)=0]'
+                )
+            ]
+            plaats, duur, uren, start, deadline = main_info[:5]
 
-                try:
-                    if wensen == []:
-                        wensen_elements = driver.find_elements(By.XPATH, '/html/body/main/div[1]/div/div[1]/div[2]/div[3]/ul[2]')
-                        wensen = [element.text for element in wensen_elements][0].split('\n')
-                except:
-                    pass
-                
-                competenties_elements = driver.find_elements(By.XPATH, '//div[h3[text()="Competenties:"]]/ul//li')
-                competenties = [element.text for element in competenties_elements]
-                if competenties == []:
-                    try:
-                         competenties = driver.find_elements(By.XPATH, '//div[h3[text()="Competenties:"]]')[0].text.split('\n')
-                    except:
-                        pass
-                title = driver.find_element(By.XPATH, '/html/body/main/section[2]/div/div/div/div/div[1]/a/h1').text
-                row = {
-                    "UID": UID,
-                    "titel":title,
-                    "plaats": plaats,
-                    "duur": duur,
-                    "uren": uren, 
-                    "text":vacature_text_elements,
-                    "start": "".join(c for c in start if c.isdigit() or c == '-'),
-                    "deadline": "".join(c for c in deadline if c.isdigit() or c == '-'),
-                    "eisen": eisen,
-                    "wensen": wensen,
-                    "competenties": competenties
-                }
-                df_rows.append(row)
-    
-    if len(df_rows)>1:
-        new_df = pd.DataFrame(df_rows).set_index('UID')
+            vacature_text = driver.find_element(
+                By.CSS_SELECTOR,
+                'div.c-vacancy-paragraph__body-text.body-text > div:nth-child(1)'
+            ).text
 
-    # MERGE + UPDATE
-    return new_df
+            eisen = [e.text for e in driver.find_elements(By.XPATH, '//div[h3[text()="Eisen:"]]/ul//li')]
+            wensen = [e.text for e in driver.find_elements(By.XPATH, '//div[h3[text()="Wensen:"]]/ul//li')]
+            competenties = [e.text for e in driver.find_elements(By.XPATH, '//div[h3[text()="Competenties:"]]/ul//li')]
+
+            title = driver.find_element(By.XPATH, '//h1').text
+
+            rows.append({
+                "UID": UID,
+                "titel": title,
+                "plaats": plaats,
+                "duur": duur,
+                "uren": uren,
+                "text": vacature_text,
+                "start": "".join(c for c in start if c.isdigit() or c == '-'),
+                "deadline": "".join(c for c in deadline if c.isdigit() or c == '-'),
+                "eisen": eisen,
+                "wensen": wensen,
+                "competenties": competenties
+            })
+
+    df = pd.DataFrame(rows).set_index("UID")
+    return df
 
 def main():
     options = Options()
-    options.add_argument("--headless=new")   # Headless voor CI
+    options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-    with driver as driver:
-        go_to_main_page_search_term_data(driver)
-        new_df_data = scrape(driver=driver)
-        go_to_main_page_search_term_data_engineer(driver=driver)
-        new_df_data_engineer = scrape(driver=driver)
 
-        new_df = pd.concat([new_df_data,new_df_data_engineer])
-        df = new_df
+    with driver:
+        go_to(driver, "data")
+        df_data = scrape(driver)
 
-    print(df)
-    df = df.loc[~df.index.duplicated(keep='first'), :]
-    with open("./circle8.json", "w") as f:
-        f.write(df.to_json(orient="index", indent=4))
+        go_to(driver, "data engineer")
+        df_data_engineer = scrape(driver)
 
-    driver.quit()
+        go_to(driver, "machine learning engineer")
+        df_ml_engineer = scrape(driver)
+
+    # Combine datasets
+    df = pd.concat([df_data, df_data_engineer, df_ml_engineer])
+
+    # Duplicates verwijderen
+    df = df[~df.index.duplicated(keep="first")]
+
+    # JSON opslaan
+    df.to_json("circle8.json", orient="index", indent=4)
+
+    print("JSON opgeslagen als circle8.json")
 
 if __name__ == "__main__":
     main()
