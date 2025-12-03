bfb7e8ad2df7a55c414795f74b8fe5addd66b6e5
wy85-bit <149866791+wy85-bit@users.noreply.github.com>
2025-12-03 12:20:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,24 +8,18 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.os_manager import ChromeType
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
 options = Options()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-options.add_argument('--disable-gpu')
-options.add_argument('--disable-extensions')
-options.add_argument('--start-maximized')
-# Memory optimization
-options.add_argument('--disk-cache-size=1')
-options.add_argument('--media-cache-size=1')
-options.add_argument('--incognito')
-options.add_argument('--remote-debugging-port=9222')
-options.add_argument('--aggressive-cache-discard')
+options.add_argument("--headless=new")
 
-service = Service(ChromeDriverManager().install())
-    
-driver = webdriver.Chrome(service=service, options=options)
+driver = webdriver.Chrome(
+    service=Service(ChromeDriverManager().install()),
+    options=options
+)
 driver.get('www.google.com')
 
 driver.quit()
